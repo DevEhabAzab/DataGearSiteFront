c70fd4a4baada74b5291c516b558ed7f0630fc31
Ehab Azab <ehab.azab@datagearbi.com>
2025-02-27 23:59:55 +0200

    change port of back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_URL: 'https://qian-api.hirebangladeshi.com', // Use production URL here such as where the Strapi CMS Hosted.
-    Media_Base_URL:'https://localhost:7062'
+    API_URL: 'http://localhost:4121/api', // Use production URL here such as where the Strapi CMS Hosted.
+    Media_Base_URL:'http://localhost:4121'
 
 };
